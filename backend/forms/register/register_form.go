214dd5dca40049802f42a7d8cef3aa9c43008a75
bongo <bongo@moove.co.jp>
2022-07-09 11:40:39 +0900

    :wrench: formを反映

--- backend/forms/register/register_form.go
+++ backend/forms/register/register_form.go
@@ -1,7 +1,12 @@
 package forms
 
 type RegisterUrlForm struct {
+    ID string `json:"id" validate:"required"`
 }
 
 type RegisterForm struct {
+    TmpID    string `json:"tmpId" validate:"required"`
+    Name     string `json:"name" validate:"required"`
+    Email    string `json:"email" validate:"required,email,max=255"`
+    Password string `json:"password" validate:"required,min=6,max=20"`
 }
--- backend/forms/register_email/register_email_form.go
+++ backend/forms/register_email/register_email_form.go
@@ -1,6 +1,6 @@
 package forms
 
 type RegisterEmailForm struct {
-    Email    string `json:"email" validate:"required,email,max=255"`
-    Password string `json:"password" validate:"required"`
+    Email        string `json:"email" validate:"required,email,max=255"`
+    RedirectPath string `json:"redirectPath" validate:"max=255"`
 }
