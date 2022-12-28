b022666531a869b4a82feb66c271a746acad715d
bongo <bongo@moove.co.jp>
2022-12-28 16:08:34 +0900

    :wrench: API側でCookieのJWTをチェックするようにする

--- backend/server/routes/router.go
+++ backend/server/routes/router.go
@@ -1,46 +1,48 @@
 package routes
 
 import (
-    "github.com/labstack/echo/v4"
-    "NagoBackend/config"
-    "NagoBackend/controllers"
-    "NagoBackend/handlers"
+	"github.com/labstack/echo/v4"
+	"NagoBackend/config"
+	"NagoBackend/constants"
+	"NagoBackend/controllers"
+	"NagoBackend/handlers"
 
-    "github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func InitRouter(e *echo.Echo) {
-    api := e.Group("/api")
-    // health
-    healthController := controllers.NewHealthController()
-    api.GET("/health", healthController.Index)
+	api := e.Group("/api")
+	// health
+	healthController := controllers.NewHealthController()
+	api.GET("/health", healthController.Index)
 
-    // register email
-    registerEmailController := controllers.NewRegisterEmailController()
-    api.POST("/register_email", registerEmailController.RegisterEmail)
+	// register email
+	registerEmailController := controllers.NewRegisterEmailController()
+	api.POST("/register_email", registerEmailController.RegisterEmail)
 
-    // register url check
-    registerController := controllers.NewRegisterController()
-    api.POST("/register_url_check", registerController.UrlCheck)
-    // register
-    api.POST("/register", registerController.Register)
+	// register url check
+	registerController := controllers.NewRegisterController()
+	api.POST("/register_url_check", registerController.UrlCheck)
+	// register
+	api.POST("/register", registerController.Register)
 
-    // login
-    loginController := controllers.NewLoginController()
-    api.POST("/login", loginController.Login)
-    // logout
-    api.GET("/logout", loginController.Logout)
+	// login
+	loginController := controllers.NewLoginController()
+	api.POST("/login", loginController.Login)
+	// logout
+	api.GET("/logout", loginController.Logout)
 
-    conf := config.GetConfig()
-    jwtConfig := middleware.JWTConfig{
-        Claims:     &handlers.JwtCustomClaims{},
-        SigningKey: []byte(conf.GetString("session.secret")),
-    }
-
-    // user
-    apiU := e.Group("/api/user")
-    apiU.Use(middleware.JWTWithConfig(jwtConfig))
-    // mypage
-    userMyinfoController := controllers.NewUserMyinfoController()
-    apiU.GET("/myinfo", userMyinfoController.Index)
+	// cookie で管理されている JWT をチェックする
+	conf := config.GetConfig()
+	jwtConfig := middleware.JWTConfig{
+		Claims:     &handlers.JwtCustomClaims{},
+		SigningKey: []byte(conf.GetString("session.secret")),
+		TokenLookup: "cookie:" + constants.SESSION_COOKIE_KEY_NAME,
+	}
+	// user
+	apiU := e.Group("/api/user")
+	apiU.Use(middleware.JWTWithConfig(jwtConfig))
+	// mypage
+	userMyinfoController := controllers.NewUserMyinfoController()
+	apiU.GET("/myinfo", userMyinfoController.Index)
 }
