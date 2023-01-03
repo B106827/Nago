36d745f1fa9ca2f35eb58f15a77a03c712453b2b
bongo <bongo@moove.co.jp>
2023-01-03 15:29:23 +0900

    :wrench: CSRF対策

--- backend/constants/constants.go
+++ backend/constants/constants.go
@@ -1,8 +1,12 @@
 package constants
 
 const (
-    // user
-    USER_STATUS_REGISTERED = 1
-    // session（Cookie）
-    SESSION_COOKIE_KEY_NAME = "nago-cs"
+	// user
+	USER_STATUS_REGISTERED = 1
+	// session（Cookie）
+	SESSION_COOKIE_KEY_NAME = "nago-cs"
+	// CSRF（Contexst）
+	CSRF_CONTEXT_KEY_NAME = "nago-ctc"
+	// CSRF（Cookie）
+	CSRF_COOKIE_KEY_NAME = "nago-cc"
 )
--- backend/server/middlewares/initialize.go
+++ backend/server/middlewares/initialize.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"NagoBackend/config"
+	"NagoBackend/constants"
 	"net/http"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
@@ -19,6 +20,12 @@ func InitMiddleware(e *echo.Echo) {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{conf.GetString("url.front"), conf.GetString("url.server")},
 		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+	}))
+	// CSRF
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		ContextKey:  constants.CSRF_CONTEXT_KEY_NAME, // contexstに保存するキー名
+		CookieName:  constants.CSRF_COOKIE_KEY_NAME,
+		TokenLookup: "cookie:" + constants.CSRF_COOKIE_KEY_NAME, //
 	}))
 	// バリデータ
 	e.Validator = &CustomValidator{}
