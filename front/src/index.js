001506c7ee6b65d46e4c852cef3bbcd621282595
bongo <bongo@moove.co.jp>
2023-01-03 19:14:45 +0900

    :wrench: 認証状態の永続化完了

--- front/src/index.js
+++ front/src/index.js
@@ -8,10 +8,14 @@ import { MuiThemeProvider } from '@material-ui/core';
 import { theme } from './assets/theme';
 import App from './App';
 import { windowResizeAction } from './reducks/utils/actions';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // 下記で store が作られる
 const history = History.createBrowserHistory();
 export const store = createStore(history);
+
+const persistor = persistStore(store);
 
 // 画面サイズを監視
 window.addEventListener('resize', () => {
@@ -24,11 +28,13 @@ ReactDOM.render(
   // React に store が使えるようにしている
   // AppコンポーネントをProviderやConnectedRouterでラッピングしている
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <MuiThemeProvider theme={theme}>
-        <App />
-      </MuiThemeProvider>
-    </ConnectedRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <MuiThemeProvider theme={theme}>
+          <App />
+        </MuiThemeProvider>
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
