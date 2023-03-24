ce0444766f634f8eb65a95a995221216988aa88d
b106827 <roomikiru@gmail.com>
2023-03-24 18:56:37 +0900

    :wrench: 未認証のユーザーが認証必須のページに訪れた場合にメッセージを表示するように修正

--- front/src/Auth.jsx
+++ front/src/Auth.jsx
@@ -1,9 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getIsLogedIn } from './reducks/users/selectors';
+import { showMessageAction } from './reducks/messages/actions';
 
 const Auth = ({ children }) => {
+  const dispatch = useDispatch();
   const selector = useSelector((state) => state);
   const isLogedIn = getIsLogedIn(selector);
+
+  useEffect(() => {
+    if (!isLogedIn) {
+      dispatch(showMessageAction('error', 'ログインしてください'));
+    }
+  }, []);
 
   if (!isLogedIn) {
     return <></>;
